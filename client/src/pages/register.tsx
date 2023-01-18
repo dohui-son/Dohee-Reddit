adbf921ea57129ef934bdcacc7ac881f70f095cf
doheeMac <thsehgml37@korea.ac.kr>
2023-01-18 21:45:36 +0900

    Fix: client register page inputs added

--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
+import InputGroup from "../components/InputGroup";
 
-const register = () => {
+const Register = () => {
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState<any>({}); // any type 추가
+
   return (
     <div className="bg-white">
       <div className="flex flex-col items-center justify-center h-screen p-6">
         <div className="w-10/12 mx-auto md:d-96">
-          <h1 className="mb-2 text-lg font-medium">회원가입</h1>
+          <h1 className="mb-2 text-lg font-medium">📋 회원가입</h1>
           <form>
+            <InputGroup
+              placeholder="Email"
+              value={email}
+              setValue={setEmail}
+              error={errors.email}
+            />
+            <InputGroup
+              placeholder="Username"
+              value={username}
+              setValue={setUsername}
+              error={errors.username}
+            />
+            <InputGroup
+              placeholder="Password"
+              value={password}
+              setValue={setPassword}
+              error={errors.password}
+            />
             <button className="w-full py-2 mb-1 text-xs font-bold text-white uppercase bg-gray-400 bordder border-gray-400 rounded">
               회원가입
             </button>
@@ -24,4 +48,4 @@ const register = () => {
   );
 };
 
-export default register;
+export default Register;

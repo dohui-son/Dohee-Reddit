d1c594c955406da1054c0a2bff9c358463ae2eb7
doheeMac <thsehgml37@korea.ac.kr>
2023-01-18 22:08:34 +0900

    Refractor: client server base url added

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,7 +1,9 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import "tailwindcss/tailwind.css";
+import Axios from "axios";
 
 export default function App({ Component, pageProps }: AppProps) {
+  Axios.defaults.baseURL = process.env.NEXT_PUBLIC_SERVER_BASE_URL + "/api"; // NEXT_PUBLIC_SERVER_BASE_URL는 NEXT JS 환경변수
   return <Component {...pageProps} />;
 }

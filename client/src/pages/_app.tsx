f178cb9628674b532ac04b81c78b278a1a59b4f4
doheeMac <thsehgml37@korea.ac.kr>
2023-01-19 12:51:25 +0900

    Feat: client context for user info updated

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -2,8 +2,13 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import "tailwindcss/tailwind.css";
 import Axios from "axios";
+import { AuthProvider } from "../context/auth";
 
 export default function App({ Component, pageProps }: AppProps) {
   Axios.defaults.baseURL = process.env.NEXT_PUBLIC_SERVER_BASE_URL + "/api"; // NEXT_PUBLIC_SERVER_BASE_URL는 NEXT JS 환경변수
-  return <Component {...pageProps} />;
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  );
 }

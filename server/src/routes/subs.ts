383b57af427ffeae967a1345cf43472e0177d0f0
doheeMac <thsehgml37@korea.ac.kr>
2023-01-19 15:50:33 +0900

    Feat: server subs middleware updated

--- server/src/routes/subs.ts
+++ server/src/routes/subs.ts
@@ -1,18 +1,13 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
 import User from "../entities/User";
+import userMiddleware from "../middlewares/user";
+import authMiddleware from "../middlewares/auth";
 
-const createSub = async (req: Request, res: Response) => {
+const createSub = async (req: Request, res: Response, next) => {
   const { name, title, description } = req.body;
-
-  const token = req.cookies.token;
-  if (!token) return next();
-
-  const { username } = jwt.verify(token, process.env.JWT_SECRET);
-  const user = await User.findOneBy({ username });
-  if (!user) throw new Error("Unauthenticated.");
 };
 const router = Router();
 
-router.post("/", createSub);
+router.post("/", userMiddleware, authMiddleware, createSub);
 
 export default router;

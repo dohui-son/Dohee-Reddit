167737e5427c74dd1cfddd67952ad9918e7aba8c
doheeMac <thsehgml37@korea.ac.kr>
2023-01-21 16:20:59 +0900

    Feat: client navbar existence depending on address

---
+++ client/src/components/NavBar.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NavBar: React.FC = () => {
+  return <div>NAVBAR</div>;
+};
+
+export default NavBar;
--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -3,14 +3,23 @@ import type { AppProps } from "next/app";
 import "tailwindcss/tailwind.css";
 import Axios from "axios";
 import { AuthProvider } from "../context/auth";
+import { useRouter } from "next/router";
+import NavBar from "../components/NavBar";
 
 export default function App({ Component, pageProps }: AppProps) {
   Axios.defaults.baseURL = process.env.NEXT_PUBLIC_SERVER_BASE_URL + "/api"; // NEXT_PUBLIC_SERVER_BASE_URL는 NEXT JS 환경변수
   Axios.defaults.withCredentials = true;
 
+  const { pathname } = useRouter();
+  const authRoutes = ["/register", "/login"];
+  const authRoute = authRoutes.includes(pathname);
+
   return (
     <AuthProvider>
-      <Component {...pageProps} />
+      {!authRoute && <NavBar />}
+      <div className={authRoute ? "" : "pt-12"}>
+        <Component {...pageProps} />
+      </div>
     </AuthProvider>
   );
 }

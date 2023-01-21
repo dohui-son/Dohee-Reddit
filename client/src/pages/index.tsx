8d48cfd07fac1ee162c56f07dd8d27676d5b8640
doheeMac <thsehgml37@korea.ac.kr>
2023-01-21 19:09:53 +0900

    Feat: client sidebar with popular commmunity list and create community button

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -2,12 +2,32 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "../styles/Home.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      INDEX
-      <h1 className="text-3xl font-bold uderline">Hello</h1>
+    <div className="flex max-w-5xl px-4 pt-5 mx-auto">
+      <div className="w-full md:mr-3 md:w-8/12">포스트리스트</div>
+
+      {/* 사이드바 */}
+      <div className="hidden w-4/12 ml-3 md:block">
+        <div className="bg-white border rounded">
+          <div className="p-4 border-b">
+            <p className="text-lg font-semibold text-centered">인기 커뮤니티</p>
+          </div>
+          {/* 커뮤니티목록 */}
+          <div></div>
+
+          <div className="w-full py-6 text-center">
+            <Link
+              href="/subs/create"
+              className="w-full p-2 text-center text-white bg-gray-400 rounded"
+            >
+              커뮤니티 만들기
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }

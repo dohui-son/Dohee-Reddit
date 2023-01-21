0c3273fbcf4ce1efc1081db3294cd9811aa47bae
doheeMac <thsehgml37@korea.ac.kr>
2023-01-21 17:19:13 +0900

    Feat: client navigation bar updated

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,7 +1,50 @@
 import React from "react";
+import Link from "next/link";
+import { useAuthState } from "../context/auth";
 
 const NavBar: React.FC = () => {
-  return <div>NAVBAR</div>;
+  const { loading, authenticated } = useAuthState();
+  return (
+    <div className="fixed inset-x-0 top-0 z-10 flex items-center justify-between h-16 px-5 bg-white">
+      <span className="text-2xl font-semibold text-gray-400">
+        <Link href="/">COMMUNITY</Link>
+      </span>
+
+      <div className="max-w-full px-4">
+        <div className="relative flex items-center bg-gray-100 border rounded hover:border-gray-700 hover:bg-white">
+          <i className="pl-4 pr-3 text-gray-400 fas fa-search"></i>
+          <input
+            type="text"
+            placeholder="Search"
+            className="px-3 py-1 bg-transparent rounded focus:outline-none"
+          />
+        </div>
+      </div>
+
+      <div className="flex">
+        {!loading && authenticated ? (
+          <button className=" w-20 p-2 text-center  mr-2 text-white bg-gray-400 rounded">
+            LOGOUT
+          </button>
+        ) : (
+          <>
+            <Link
+              href="/login"
+              className="w-20 p-2 mr-2 text-center text-blue-500 border border-blue-500 rounded"
+            >
+              LOGIN
+            </Link>
+            <Link
+              href="/register"
+              className="w-20 p-2 text-center text-white bg-gray-400 rounded"
+            >
+              SIGNUP
+            </Link>
+          </>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;

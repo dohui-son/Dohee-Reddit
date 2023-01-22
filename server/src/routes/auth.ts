bb76807dbb300f2d70a2d5ddcc40aca07a220d27
doheeMac <thsehgml37@korea.ac.kr>
2023-01-22 23:54:21 +0900

    Feat: server cookie expiration period updated

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -98,7 +98,7 @@ const login = async (req: Request, res: Response) => {
       "Set-Cookie",
       cookie.serialize("token", token, {
         httpOnly: true,
-        maxAge: 60 * 60 * 24 * 2,
+        maxAge: 60 * 60 * 24 * 0.3,
         path: "/",
       })
     );

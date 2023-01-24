7472ae801778500cc57ea8cba5b6ad498b23ba3f
doheeMac <thsehgml37@korea.ac.kr>
2023-01-24 10:40:10 +0900

    Feat: server top 5 community list

--- server/src/routes/subs.ts
+++ server/src/routes/subs.ts
@@ -1,6 +1,7 @@
 import { Router, Request, Response } from "express";
 import User from "../entities/User";
 import Sub from "../entities/Sub";
+import Post from "../entities/Post";
 import userMiddleware from "../middlewares/user";
 import authMiddleware from "../middlewares/auth";
 import { isEmpty, validate } from "class-validator";
@@ -45,8 +46,31 @@ const createSub = async (req: Request, res: Response, next) => {
     return res.status(500).json({ error: "문제가 발생했습니다." });
   }
 };
+
+const topSubs = async (_: Request, res: Response) => {
+  try {
+    const imageUrlExp = `COALESCE(s."imageUrn", 'https://www.gravatar.com/avatar?d=mp&f=y')`;
+    const subs = await AppDataSource.createQueryBuilder()
+      .select(
+        `s.title, s.name, ${imageUrlExp} as "imageUrl", count(p.id) as "postCount"`
+      )
+      .from(Sub, "s")
+      .leftJoin(Post, "p", `s.name = p."subName"`)
+      .groupBy('s.title, s.name, "imageUrl"')
+      .orderBy(`"postCount"`, "DESC")
+      .limit(5)
+      .execute();
+
+    return res.json(subs);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "Something went Wrong." });
+  }
+};
+
 const router = Router();
 
 router.post("/", userMiddleware, authMiddleware, createSub);
+router.get("/sub/topSubs", topSubs);
 
 export default router;

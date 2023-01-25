0f04382d79302e21c3042e3cdd48ff9af61cc279
doheeMac <thsehgml37@korea.ac.kr>
2023-01-25 20:10:05 +0900

    Feat: client sub detail page ui

--- client/src/pages/r/[sub].tsx
+++ client/src/pages/r/[sub].tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useRouter } from "next/router";
-import React from "react";
+import React, { Fragment } from "react";
 import useSWR from "swr";
 
 const SubPage = () => {
@@ -22,7 +22,32 @@ const SubPage = () => {
     fetcher
   );
 
-  return <h1>SUBPAGE</h1>;
-};
+  return <>{sub && 
+  <Fragment>
+    {/* Sub Detail info and images */}
+    <div className={cls("bg-gray-400")}>
+        {/* Banner Image */}
+        {sub.bannerUrl?(<div className="h-56"
+        style={{backgroundImage: `url(${sub.bannerUrl})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        backgroundPosition: "center"
+        }}
+        onClick={()=> openFileInput("banner")}
+        ></div>):(
+        <div className="h-20 bg-gray-400"></div>)}
+    </div>
+    {/* Sub meta data */}
+    <div className="h-20 bg-white">
+<div className="relative flex max-w-5xl px-5 mx-auto">
+    <div className="absolute" style={{top:-15}}>
+        {sub.imageUrl && (<Image src={sub.imageUrl} alt="" width={70} height={70} onClick={()=> openFileInput("image")} className={cls("rounded-full")}/>)}
+
+    </div>
+</div>
+    </div>
+    
+  </Fragment>;
+}</>;
 
 export default SubPage;

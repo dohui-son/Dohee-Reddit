80f1bbdcdf6ef874ec470d32d50d40cd05e21ac1
doheeMac <thsehgml37@korea.ac.kr>
2023-01-25 17:25:39 +0900

    Feat: client sub detailpage routings

--- client/src/pages/r/[sub].tsx
+++ client/src/pages/r/[sub].tsx
@@ -1,6 +1,26 @@
+import axios from "axios";
+import { useRouter } from "next/router";
 import React from "react";
+import useSWR from "swr";
 
 const SubPage = () => {
+  const router = useRouter();
+  const subName = router.query.sub;
+
+  const fetcher = async (url: string) => {
+    try {
+      const res = await axios.get(url);
+      return res.data;
+    } catch (error: any) {
+      throw error.response.data;
+    }
+  };
+
+  const { data: sub, error } = useSWR(
+    subName ? `/sub/${subName}` : null,
+    fetcher
+  );
+
   return <h1>SUBPAGE</h1>;
 };
 

d26ea0cd78e47c3d50f2f4840697ba95684ecc53
doheeMac <thsehgml37@korea.ac.kr>
2023-01-27 21:46:34 +0900

    Feat: server create posts feature

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,6 +3,7 @@ import morgan from "morgan";
 import { AppDataSource } from "./data-source";
 import authRoutes from "./routes/auth";
 import subRoutes from "./routes/subs";
+import postRoutes from "./routes/posts";
 import cors from "cors";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -26,6 +27,7 @@ dotenv.config(); // 환경변수 사용하기
 app.get("/", (_, res) => res.send("running"));
 app.use("/api/auth", authRoutes);
 app.use("/api/subs", subRoutes);
+app.use("/api/posts", postRoutes);
 
 let port = 4000;
 
